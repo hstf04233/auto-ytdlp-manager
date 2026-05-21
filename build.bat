c59a8b4858071a622ac4b8d965551ab47c384054
Bobby <202839055+BobbySnook@users.noreply.github.com>
2026-05-20 23:57:04 -0500

    CGO_ENABLED is now enabled when building

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 
+set CGO_ENABLED=1
 go build -ldflags="-s -w" -gcflags="-N -l" .
 
 if %ERRORLEVEL% NEQ 0 (
--- build_debug.bat
+++ build_debug.bat
@@ -1,5 +1,6 @@
 @echo off
 
+set CGO_ENABLED=1
 go build -ldflags "-X 'main.APPLICATION_VERSION=debug'" -o autoytdlpmanager-debug.exe .
 
 if %ERRORLEVEL% NEQ 0 (
