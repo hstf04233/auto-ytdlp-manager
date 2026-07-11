b4f39169973787f32dd94f7595dc25ff781833b4
hstf04233 <hstf04233@gmail.com>
2026-07-11 10:15:23 -0500

    Move old logs to ./logs/

--- logprint.go
+++ logprint.go
@@ -12,6 +12,10 @@ var (
 	logSync sync.Mutex
 )
 
+const (
+	OLDLOGS_DIRECTORY = "./logs"
+)
+
 func L_Printf(format string, a ... any) {
 	TimeNow := time.Now().Local().Format("2006-01-02 15:04:05")
 	Msg := fmt.Sprintf(format, a ...)
@@ -28,7 +32,45 @@ func L_Printf(format string, a ... any) {
 	}()
 }
 
+func MoveCurrentLog() error {
+	if DoesFileExist("log_current.log") {
+		File, err := os.Open("log_current.log")
+		if err != nil {
+			File.Close()
+			fmt.Printf("Could not open old log file, error: %v\n", err)
+			return err
+		}
+		Info, err := File.Stat()
+		if err != nil {
+			File.Close()
+			fmt.Printf("Could not get file info for old log file, error: %v\n", err)
+			return err
+		}
+		
+		ModTime := Info.ModTime()
+		File.Close()
+		
+		err = os.MkdirAll(OLDLOGS_DIRECTORY, 0755)
+		if err != nil {
+			fmt.Printf("Could not create file directory \"%s\" , error: %v\n", OLDLOGS_DIRECTORY, err)
+			return err
+		}
+		
+		NewFilePath := fmt.Sprintf("%s/%s.log", OLDLOGS_DIRECTORY, ModTime.Format("2006-01-02 15-04-05"))
+		
+		err = os.Rename("log_current.log", NewFilePath)
+		if err != nil {
+			fmt.Printf("Could not move log file \"log_current.log\" to \"%s\", error: %v\n", NewFilePath, err)
+			return err
+		}
+	}
+	
+	return nil
+}
+
 func InitLogPrint() error {
+	MoveCurrentLog()
+	
 	var err error
 	logFile, err = os.Create("log_current.log")
 	if err != nil {
