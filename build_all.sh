f3d9231a4a27618504929615ffe370530503e999
hstf04233 <hstf04233@gmail.com>
2026-09-02 15:49:17 -0500

    add build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-CGO_ENABLED=1
-
 if command -v windres >/dev/null; then
     windres app.rc -O coff -o app.syso
 else
     echo "windres not installed. Windows binaries will build without icons!" >&2
+	rm -rf "./app.syso"
 fi
 
 # Windows
-GOOS=windows
-# amd64
-GOARCH=amd64
-go build -ldflags="-s -w" -gcflags="-N -l" -o "./builds/autoytdlpmanager.exe"
-# arm64
-GOARCH=arm64
-go build -ldflags="-s -w" -gcflags="-N -l" -o "./builds/autoytdlpmanager_arm64.exe"
+echo Building Windows amd64...
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -gcflags="-N -l" -o "./builds/autoytdlpmanager.exe"
+echo Done
+
+echo Building Windows arm64...
+CGO_ENABLED=1 GOOS=windows GOARCH=arm64 go build -ldflags="-s -w" -gcflags="-N -l" -o "./builds/autoytdlpmanager_arm64.exe"
+echo Done
 
 # Linux
-GOOS=windows
-GOARCH=amd64
-go build -ldflags="-s -w" -gcflags="-N -l" -o "./builds/autoytdlpmanager"
-# arm64
-GOARCH=arm64
-go build -ldflags="-s -w" -gcflags="-N -l" -o "./builds/autoytdlpmanager_arm64"
+echo Building Linux amd64...
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -gcflags="-N -l" -o "./builds/autoytdlpmanager"
+echo Done
+
+echo Building Linux arm64...
+GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -gcflags="-N -l" -o "./builds/autoytdlpmanager_arm64"
+echo Done
